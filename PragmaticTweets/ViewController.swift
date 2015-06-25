b8f2e9c67b88f827759ffc78b0c27206c3eba366
Jason Hagglund <jason.hagglund@climate.com>
2015-06-25 15:34:57 -0700

    Initial Commit

---
+++ PragmaticTweets/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PragmaticTweets
+//
+//  Created by Jason Hagglund on 6/25/15.
+//  Copyright © 2015 Jason Hagglund. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
